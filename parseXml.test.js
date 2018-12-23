5e9b1adb9838b838a13978b466b0aa7d98ba3c0f
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-12-22 23:26:51 -0800

    Wrote test for adding more than one nested tags to 'b'

--- parseXml.test.js
+++ parseXml.test.js
@@ -6,9 +6,20 @@ describe('XML correctly parsed to JSON: ', () => {
     expect(parseXml('<a>bla</a><b><c>yes</c><d>no</d></b>')).toEqual(expected);
   });
 
-  it('parseXml function should return the same value when there is more than one non nested tags', () => {
+  it('adds keys when there is more than one non nested tags added to the XML', () => {
     const expected = { a: 'bla', b: { c: 'yes', d: 'no' }, p: 'paystand' };
     expect(parseXml('<a>bla</a><b><c>yes</c><d>no</d></b><p>paystand</p>')).toEqual(expected);
   });
+
+  it('adds keys when there is more than one nested tags added to the XML', () => {
+    const expected = { a: 'bla', b: { p: {e: { c: 'yes', d: 'no' }}}};
+    expect(parseXml('<a>bla</a><b><p><e><c>yes</c><d>no</d></e></p></b>')).toEqual(expected);
+  });
+
+  // it('adds keys when there is more than one nested tags added to the XML', () => {
+  //   const expected = { a: 'bla', b: { c: 'yes', d: 'no' }, p: 'paystand' };
+  //   expect(parseXml('<a>bla</a><b><c>yes</c><d>no</d></b><p><p>paystand</p></p>')).toEqual(expected);
+  // });
+
 });
 
