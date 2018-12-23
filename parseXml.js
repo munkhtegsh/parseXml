2af3f52e60a5de5d3ac3e0e269626accf01e3c81
Munkhtegsh <munkhtegsh@outlook.com>
2018-12-22 20:52:32 -0800

    Created parseXml.js and added my solution

---
+++ parseXml.js
@@ -0,0 +1,46 @@
+// 1. filter the keys
+// 2. filter the contents
+// 3. make recursive calls, if '<' tag exists after the '>' tag
+
+function getKey(str) {
+  let key = '';
+  for (let char of str) {
+    if (char === '>') {
+      return key;
+    } else {
+      key += char
+    }
+  }
+}
+
+// getKey('a>')
+
+function getContent(str, key) {
+  let endTagIdx = str.indexOf(`</${key}>`);
+  return str.substring(0, endTagIdx);
+}
+
+// getContent('bla</a>', 'a');
+
+function parseXml(str) {
+  let dict = {};
+  if (str.indexOf("<") == -1) {
+    return str
+  }
+  let i = 0;
+  while (i < str.length) {
+    if (str[i] === '<') {
+      let key = getKey(str.substring(i + 1));
+      str = str.substring(i + key.length + 2);
+      let content = getContent(str, key);
+      str = str.substring(content.length + key.length + 3);
+      const dictValues = parseXml(content);
+
+      dict[key] = dictValues;
+    }
+  }
+  return dict;
+}
+parseXml('<a>bla</a><b><c>yes</c><d>no</d></b>');
+
+module.exports = parseXml;
